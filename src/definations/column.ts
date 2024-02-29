f0a28a2700d7cc021d25544f20637f94ac5e8a13
Suvi <suvi.julkunen@tuni.fi>
2024-02-29 14:12:42 +0200

    Define row, column, cellGroup, formatter, keyTypes

--- src/definations/column.ts
+++ src/definations/column.ts
@@ -1,5 +1,21 @@
 import CellGroup from "./cellGroup.ts";
+import CellStyle from "./cellStyle";
+import { CellKey }from "./keyTypes";
+import { ColumnKey } from "./keyTypes";
+import { RowKey } from "./keyTypes";
 
-export default class Column extends CellGroup {
+export default class Column extends CellGroup<ColumnKey> {
+    cellIndex: Map<RowKey, CellKey>;
+    cellFormatting: Map<RowKey, CellStyle>;
 
+    constructor(width: number, position: number) {
+        super(width, position);
+        this.key = new ColumnKey();
+        this.cellIndex = new Map<RowKey, CellKey>();
+        this.cellFormatting = new Map<RowKey, CellStyle>();
+    }
+
+    getWidth(): number {
+        return this.size_;
+    }
 }
