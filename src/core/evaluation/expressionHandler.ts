b7df9a264c304b3a6643d8f290895bd37100bf10
Fluglow <38987331+Fluglow@users.noreply.github.com>
2024-03-12 15:24:04 +0200

    Add basic evaluation logic

--- src/core/evaluation/expressionHandler.ts
+++ src/core/evaluation/expressionHandler.ts
@@ -1,5 +1,12 @@
+import { parse } from "mathjs";
+
 export default class ExpressionHandler {
   static evaluate(expression: string): string {
-    return expression;
+    if (!expression.startsWith("=")) return expression;
+    expression = expression.substring(1);
+
+    const parsed = parse(expression);
+    const evaluated = parsed.evaluate(); // TODO This will crash on unexpected symbols.
+    return evaluated;
   }
 }
