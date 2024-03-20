6df8b73282747fcfe428a827a21fe10e4a8d9004
Rafin Akther Utsahw <rafin.utshaw@tuni.fi>
2024-03-20 17:50:00 +0200

    Add empty table view

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,17 @@ export default class LightSheet {
   sheet: Sheet;
   onCellChange?;
 
+  #defaultRowCount: number = 4;
+  #defaultColCount: number = 4;
+
   constructor(targetElement: Element, options: LightSheetOptions) {
     this.options = options;
     this.sheet = new Sheet();
     this.ui = new UI(
       targetElement,
       this,
-      this.options.data.length,
-      this.options.data[0].length,
+      this.options.data.length ?? this.#defaultRowCount,
+      this.options.data.length ? this.options.data[0].length : this.#defaultColCount
     );
     this.initializeData();
     if (options.onCellChange) {
@@ -29,21 +32,29 @@ export default class LightSheet {
   }
 
   initializeData() {
+    debugger
     // Create header row and add headers
+    const colLength = (this.options.data && this.options.data[0]?.length) ? this.options.data[0]?.length : this.#defaultColCount
+    const rowLength = (this.options.data && this.options.data?.length) ? this.options.data?.length : this.#defaultRowCount
     const headerData = Array.from(
-      { length: this.options.data[0].length + 1 }, // Adding 1 for the row number column
+      { length: colLength + 1 }, // Adding 1 for the row number column
       (_, i) => (i === 0 ? "" : this.generateRowLabel(i)), // Generating row labels
     );
     this.ui.addHeader(headerData);
 
-    for (let i = 0; i < this.options.data.length; i++) {
-      const item = this.options.data[i];
+    for (let i = 0; i < rowLength; i++) {
+      let item, itemCount = this.#defaultColCount;
+      if (this.options.data && this.options.data.length) {
+        item = this.options.data[i]
+        itemCount = item.length
+      }
+
       //create new row
       const rowDom = this.ui.addRow(i);
 
-      for (let j = 0; j < item.length; j++) {
+      for (let j = 0; j < itemCount; j++) {
         //if data is not empty add cell to core and render ui, otherwise render only ui
-        if (item[j]) {
+        if (item && item[j]) {
           const cell = this.sheet.setCellAt(j, i, item[j]);
           const rowKeyStr = cell.position.rowKey!.toString();
           const columnKeyStr = cell.position.columnKey!.toString();
