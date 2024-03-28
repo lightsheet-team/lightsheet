1ffdf4ff2fd359bc1e2466ccb28395097a2cce21
Gimantha Dissanayake <gimantha.dissanayake@tuni.fi>
2024-03-28 16:36:11 +0200

    Create new types for multiple cell selection

--- src/ui/render.types.ts
+++ src/ui/render.types.ts
@@ -2,3 +2,13 @@ export type CellIdInfo = {
   keyParts: string[];
   isIndex: boolean;
 };
+
+export type SelectionContainer = {
+  selectionStart: Coordinate | null;
+  selectionEnd: Coordinate | null;
+}
+
+export type Coordinate = {
+  rowPosition: number;
+  columnPosition: number;
+}
